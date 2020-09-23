c61192e4f10853743eef2815b1d85206d8ff89e8
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-09-23 13:39:45 -0400

    change to new driver

--- humiditySensor.go
+++ humiditySensor.go
@@ -3,28 +3,26 @@ package main
 import (
 	"fmt"
 	"gobot.io/x/gobot"
-	"gobot.io/x/gobot/drivers/aio"
 	"gobot.io/x/gobot/drivers/i2c"
 	g "gobot.io/x/gobot/platforms/dexter/gopigo3"
 	"gobot.io/x/gobot/platforms/raspi"
-	"strconv"
 	"time"
 )
 
-func robotRunLoop(gopigo3 *g.Driver, humiditySensor *aio.AnalogSensorDriver, lcd *i2c.GroveLcdDriver) {
+func robotRunLoop(gopigo3 *g.Driver, humiditySensor *i2c.SHT3xDriver, lcd *i2c.GroveLcdDriver) {
 	for {
 
 		err := lcd.Clear()
 
-		humiditySensorVal, err := humiditySensor.Read()
+		temp, _, err := humiditySensor.Sample()
 
 		if err != nil {
 			fmt.Errorf("Error reading sensor %+v", err)
 		}
 
-		fmt.Println("Sensor Value is ", humiditySensorVal)
+		fmt.Println("Sensor Value is ", temp)
 
-		err = lcd.Write(strconv.Itoa(humiditySensorVal))
+		err = lcd.Write(fmt.Sprintf("%f", temp))
 
 		if err != nil {
 			fmt.Errorf("Error printing to LCD %+v", err)
@@ -38,16 +36,17 @@ func main() {
 	raspiAdaptor := raspi.NewAdaptor()
 	gopigo3 := g.NewDriver(raspiAdaptor)
 
-	humiditySensor := aio.NewAnalogSensorDriver(gopigo3, "AD_1_1")
+	//humiditySensor := aio.NewAnalogSensorDriver(gopigo3, "AD_1_1")
 	lcd := i2c.NewGroveLcdDriver(raspiAdaptor)
+	temperatureSensor := i2c.NewSHT3xDriver(raspiAdaptor)
 
 	mainRobotFunc := func() {
-		robotRunLoop(gopigo3, humiditySensor, lcd)
+		robotRunLoop(gopigo3, temperatureSensor, lcd)
 	}
 
 	robot := gobot.NewRobot("gopigo3HumiditySensor",
 		[]gobot.Connection{raspiAdaptor},
-		[]gobot.Device{gopigo3, humiditySensor, lcd},
+		[]gobot.Device{gopigo3, temperatureSensor, lcd},
 		mainRobotFunc,
 	)
 
